932a35b71cc2a5a02562fcc4586b5f453ccefdc4
bigcodefish <572401752@qq.com>
2025-06-30 14:19:51 +0800

    组长修改
    上传后端框架

---
+++ WebCarProject/src/test/java/com/example/webcarproject/WebCarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webcarproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCarProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
